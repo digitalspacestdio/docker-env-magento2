0c8ee4b699b3028095deb3a38fa10cf6b014d165
scherepanov <sergey@cherepanov.org.ua>
2023-03-07 14:21:50 +0000

    0.4.16

--- Formula/docker-compose-magento.rb
+++ Formula/docker-compose-magento.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class DockerComposeMagento < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-magento.git", :using => :git
-  version "0.4.15"
+  version "0.4.16"
   revision 1
 
   depends_on 'jq'
